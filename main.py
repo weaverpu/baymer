07d80c6e23251b00c53ca32c0de8a6b904b859e1
weaverpu <112497469+weaverpu@users.noreply.github.com>
2023-02-27 13:30:40 -0500

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from restaurant import restaurant
-from restaurantmanager import restaurantmanger
-
-a = restaurantmanger("./restaurants/AllRestuarants.txt")
-list1 = a.ReturnRestaruantList()
-a = list1[0]
-dict1 = a.ReturnMenue()
-print(dict1)
